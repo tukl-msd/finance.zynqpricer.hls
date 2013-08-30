03d08c3f6f03e97c272e48548ca0ab09b5e8ff1a
Christian Brugger <brugger@eit.uni-kl.de>
2013-08-30 23:29:08 +0000

    updated run.sh

--- heston_sl/sw/run.sh
+++ heston_sl/sw/run.sh
@@ -3,6 +3,6 @@ sync
 make init_rng run_both
 sudo chown root:admin /dev/xdevcfg; sudo chmod 660 /dev/xdevcfg
 sudo chmod 660 /dev/mem
-cat heston_sl_3x.bin > /dev/xdevcfg
-sudo ./init_rng heston_sl_3x.json
+cat ../bin/heston_sl_3x.bin > /dev/xdevcfg
+sudo ./init_rng ../bin/heston_sl_3x.json
 sudo taskset -c 1 ./run_both params_run.json
