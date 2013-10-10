551747d53ceff3b205cb3c911a71d458bd5bb3e3
Christian Brugger <brugger@eit.uni-kl.de>
2013-10-10 14:13:01 +0200

    updated simulation file

--- results/2013.10.10_feller_violation/simulation.sh
+++ results/2013.10.10_feller_violation/simulation.sh
@@ -1,7 +1,9 @@
-# based on commit 996c521ba96e6754b318b33dbeac5869061ed69c
+# based on commit d5fc7fd01bddaeb0f133206deb0d845d5d93a165
 
 # run from software directory
 
 src\feller_violation_analysis.py output
 
 src\feller_violation_analysis_gaussian.py output
+
+# run on Laptop over night for 6 hours
