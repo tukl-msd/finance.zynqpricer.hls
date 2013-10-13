ed3755aa80164299ab6a272238582dc224107ead
Christian Brugger <brugger@eit.uni-kl.de>
2013-10-13 09:35:11 +0200

    added information about 128_8

---
+++ results/2013.10.12_feller_violation_cluster_128_8/simulation.sh
@@ -0,0 +1,9 @@
+# based on commit 87c6d514d071a8812c49df6ec5814b5035eceeae
+
+# run from '../../software' directory
+
+python3 src/feller_violation_analysis_cluster_submit.py
+
+src\feller_violation_analysis_gaussian.py output
+
+# run on cluster for 1:30 hours with 1024 jobs = 64 days CPU time
