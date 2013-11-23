6cbec14b983749ea589f5b05cab848558612e17e
Christian Brugger <brugger@eit.uni-kl.de>
2013-11-23 14:13:29 +0000

    added performance test

---
+++ software/src/perf_test.cpp
@@ -0,0 +1,17 @@
+//
+// Copyright (C) 2013 University of Kaiserslautern
+// Microelectronic Systems Design Research Group
+//
+// Christian Brugger (brugger@eit.uni-kl.de)
+// 20. August 2013
+//
+
+#include <iostream>
+
+int main(int argc, char *argv[]) {
+	std::cout << "test" << std::endl;
+
+
+
+	return 0;
+}
