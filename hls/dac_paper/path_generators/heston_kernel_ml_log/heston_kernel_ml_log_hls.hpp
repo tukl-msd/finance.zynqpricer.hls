9c27e2077a293527b4f279a6cd77e400ceed77ab
Christian Brugger <brugger@eit.uni-kl.de>
2013-11-24 12:51:43 +0100

    added kernels with correleation

---
+++ hls/dac_paper/path_generators/heston_kernel_ml_log/heston_kernel_ml_log_hls.hpp
@@ -0,0 +1,57 @@
+//
+// Copyright (C) 2013 University of Kaiserslautern
+// Microelectronic Systems Design Research Group
+//
+// Christian Brugger (brugger@eit.uni-kl.de)
+// 7. September 2013
+//
+// Using: Xilinx Vivado HLS 2013.2
+//
+
+#ifndef __HESTON_KERNEL_SL_HLS_HPP__
+#define __HESTON_KERNEL_SL_HLS_HPP__
+
+#include <hls_stream.h>
+#include <ap_int.h>
+
+#include <stdint.h>
+
+#define BLOCK_SIZE 256
+
+#define calc_t float
+
+struct params_ml {
+	calc_t log_spot_price;
+	calc_t reversion_rate_TIMES_step_size_fine;
+	calc_t reversion_rate_TIMES_step_size_coarse;
+	calc_t long_term_avg_vola;
+	calc_t vol_of_vol_TIMES_sqrt_step_size_fine;
+	calc_t double_riskless_rate; // = 2 * riskless_rate
+	calc_t vola_0;
+	calc_t correlation;
+	calc_t inv_correlation; // = sqrt(1 - correlation**2)
+	// both knockout
+//	calc_t log_lower_barrier_value;
+//	calc_t log_upper_barrier_value;
+	// simulation params
+	uint32_t step_cnt_coarse; // assert step_cnt % ml_constant == 0
+	ap_uint<5> ml_constant;
+	bool do_multilevel;
+	// step_size = time_to_maturity / step_cnt
+	calc_t half_step_size_fine; // = step_size_fine / 2
+	calc_t half_step_size_coarse; // = step_size_coarse / 2
+	calc_t sqrt_step_size_fine; // = sqrt(step_size_fine)
+//	calc_t barrier_correction_factor_fine; // = BARRIER_HIT_CORRECTION * sqrt_step_size_fine
+//	calc_t barrier_correction_factor_coarse; // = BARRIER_HIT_CORRECTION * sqrt_step_size_coarse
+	uint32_t path_cnt;
+};
+
+void heston_kernel_ml_log(params_ml params, hls::stream<calc_t> &gaussian_rn1,
+		hls::stream<calc_t> &gaussian_rn2, hls::stream<calc_t> &prices);
+
+#endif
+
+
+
+
+
---
+++ hls/dac_paper/path_generators/heston_kernel_ml_real/heston_kernel_ml_real_tb.cpp
@@ -0,0 +1,159 @@
+//
+// Copyright (C) 2013 University of Kaiserslautern
+// Microelectronic Systems Design Research Group
+//
+// Christian Brugger (brugger@eit.uni-kl.de)
+// 7. September 2013
+//
+// Using: Xilinx Vivado HLS 2013.2
+//
+
+#include "heston_kernel_ml_real_hls.hpp"
+
+#include "mt19937ar.h"
+
+#include <cmath>
+#include <iostream>
+
+// continuity correction, see Broadie, Glasserman, Kou (1997)
+#define BARRIER_HIT_CORRECTION 0.5826
+
+void init_rng() {
+	init_genrand(0);
+}
+
+
+void box_muller(double &z0, double &z1) {
+	double u1 = (genrand_int32() + 1) * (1.0 / 4294967296.0);
+	double u2 = (genrand_int32() + 1) * (2.0 * M_PI / 4294967296.0);
+
+	double r = std::sqrt(-2 * std::log(u1));
+	z0 = r * std::cos(u2);
+	z1 = r * std::sin(u2);
+}
+
+void fill_gaussian_rng_stream(hls::stream<calc_t> &rns, unsigned size) {
+	double z0, z1;
+	for (unsigned i = 0; i < size; ++i) {
+		box_muller(z0, z1);
+		rns.write(z0);
+		if (++i < size)
+			rns.write(z1);
+	}
+}
+
+uint32_t round_to_next_block(uint32_t i) {
+	return ((i + BLOCK_SIZE - 1) / BLOCK_SIZE) * BLOCK_SIZE;
+}
+
+int main(int argc, char *argv[]) {
+
+	// call option
+	double spot_price = 100;
+	double reversion_rate = 0.5;
+	double long_term_avg_vola = 0.04;
+	double vol_of_vol = 1;
+	double riskless_rate = 0;
+	double vola_0 = 0.04;
+	double correlation = 0;
+	double time_to_maturity = 1;
+	double strike_price = 100;
+	// both knockout
+	double lower_barrier_value = 90;
+	double upper_barrier_value = 110;
+	// reference
+	double ref_price = 0.74870;
+	double ref_price_precision = 0.00001;
+
+	unsigned ml_constant = 4;
+	bool do_multilevel = true;
+	unsigned step_cnt = 16; // fine;
+	const unsigned path_cnt = round_to_next_block(100000); //10000;
+
+	if (step_cnt % ml_constant != 0) {
+		std::cerr << "ERROR: step_cnt % ml_constant != 0" << std::endl;
+		return 1;
+	}
+	if (path_cnt % BLOCK_SIZE != 0) {
+		std::cerr << "ERROR: path_cnt % BLOCK_SIZE != 0" << std::endl;
+		return 1;
+	}
+
+	hls::stream<calc_t> gaussian_rn1;
+	fill_gaussian_rng_stream(gaussian_rn1, step_cnt *
+			round_to_next_block(path_cnt));
+	hls::stream<calc_t> gaussian_rn2;
+	fill_gaussian_rng_stream(gaussian_rn2, step_cnt *
+			round_to_next_block(path_cnt));
+	hls::stream<calc_t> prices;
+
+	double step_size_fine = time_to_maturity / step_cnt;
+	double step_size_coarse = step_size_fine * ml_constant;
+
+	params_ml params = {
+			std::log(spot_price),
+			reversion_rate * step_size_fine,
+			reversion_rate * step_size_coarse,
+			long_term_avg_vola,
+			vol_of_vol * std::sqrt(step_size_fine),
+			2 * riskless_rate,
+			vola_0,
+			correlation,
+			// both knockout
+			std::log(lower_barrier_value),
+			std::log(upper_barrier_value),
+			// simulation params
+			step_cnt / ml_constant,
+			ml_constant,
+			do_multilevel,
+			//step_size,
+			step_size_fine / 2,
+			step_size_coarse / 2,
+			std::sqrt(step_size_fine),
+			BARRIER_HIT_CORRECTION * std::sqrt(step_size_fine),
+			BARRIER_HIT_CORRECTION * std::sqrt(step_size_coarse),
+			path_cnt};
+
+	heston_kernel_ml_real(params, gaussian_rn1, gaussian_rn2, prices);
+
+	if (prices.size() != path_cnt * (do_multilevel ? 2 : 1) + 1) {
+		std::cerr << "Wrong number of prices returned." << std::endl;
+		std::cerr << "Expected " << path_cnt * 2 + 1 <<
+				" got " << prices.size() << std::endl;
+		return -1;
+	}
+
+	int block_size = prices.read();
+	float res_prices[path_cnt * (do_multilevel ? 2 : 1)];
+	for (unsigned i = 0; i < path_cnt * (do_multilevel ? 2 : 1); ++i) {
+		res_prices[i] = std::max(0., std::exp(prices.read()) - strike_price);
+	}
+
+	// sort fine and coarse paths
+	double result_fine = 0;
+	double result_coarse = 0;
+	for (unsigned i = 0; i < path_cnt; ++i) {
+		if (do_multilevel){
+			int block = i / block_size;
+			int block_i = i % block_size;
+			float fine = res_prices[2 * block * block_size + block_i];
+			float coarse = res_prices[(2 * block + 1) * block_size + block_i];
+			result_fine += fine;
+			result_coarse += coarse;
+			//std::cout << i << " - " << fine << " - " << coarse << std::endl;
+		} else {
+			result_fine += res_prices[i];
+		}
+	}
+	result_fine /= path_cnt;
+	result_coarse /= path_cnt;
+	std::cout << "Raw difference = " << result_fine - result_coarse << std::endl;
+	result_fine *= std::exp(-riskless_rate * time_to_maturity);
+	result_coarse *= std::exp(-riskless_rate * time_to_maturity);
+
+	std::cout << "Result coarse = " << result_coarse << std::endl;
+	std::cout << "Result fine   = " << result_fine << std::endl;
+	std::cout << "Reference     = " << ref_price << std::endl;
+
+	return 0;
+}
